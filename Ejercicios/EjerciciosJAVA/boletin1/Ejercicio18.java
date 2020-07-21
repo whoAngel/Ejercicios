24325874b00628e0f41f3b45fde6cffc2da074ee
whoAngel <HP@DESKTOP-HTIUQDK>
2020-07-21 01:48:41 -0500

    2

--- Ejercicios/EjerciciosJAVA/boletin1/Ejercicio18.java
+++ Ejercicios/EjerciciosJAVA/boletin1/Ejercicio18.java
@@ -7,6 +7,40 @@ package boletin1;
 
 public class Ejercicio18 {
 	public static void main(String args[]) {
+		int dia, mes, año;
+		int diaDelMes=0;
+		System.out.print("Ingrese dia: ");
+		dia = Entrada.entero();
+		System.out.print("Ingrese mes: ");
+		mes = Entrada.entero();
+		System.out.print("Ingrese año: ");
+		año = Entrada.entero();
 		
+		try {
+		if (mes == 2) {
+			diaDelMes=28;
+		}
+		if (mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12) {
+			diaDelMes=31;
+		}
+		if (mes==4 || mes==6 || mes==9 || mes==11) {
+			diaDelMes=30;
+		}
+		
+		dia++;
+		
+		if (dia>=diaDelMes) {
+			dia=1;
+			mes++;
+			if (mes>=12) {
+				mes=1;
+				año++;
+			}
+		}
+		System.out.println(dia + "/"+ mes + "/"+ año);
+		}
+		catch(Exception e){
+			System.out.println("Fecha Erronea.");
+		}
 	}
 }
---
+++ Ejercicios/EjerciciosJAVA/boletin1/Ejercicio19.java
@@ -0,0 +1,12 @@
+package boletin1;
+
+/*
+ * Pedir 2 fechas y mostrar el dia que hay de diferencia entre ellas,
+ * suponiendo que todo los dias son de 30 dias
+ */
+
+public class Ejercicio19 {
+	public static void main(String args[]) {
+		
+	}
+}
