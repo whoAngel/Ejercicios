b231e06dd8fe58e4d794e70c41588e807ae97e09
whoAngel <HP@DESKTOP-HTIUQDK>
2020-07-22 03:54:54 -0500

    Condiciones y bucles

--- Ejercicios/EjerciciosJAVA/boletin2/Ejercicio4.java
+++ Ejercicios/EjerciciosJAVA/boletin2/Ejercicio4.java
@@ -1,9 +1,21 @@
 package boletin2;
 
 /*
- * 
+ * Pedir un numero hasta que sea negativo y mostrar cuantos 
+ * numeros se han introducido
  */
 
 public class Ejercicio4 {
-
+	public static void main(String args[]) {
+		int num, cont=0;
+		System.out.print("Ingrese un numero: ");
+		num = Entrada.entero();
+		
+		while (num>0) {
+			cont++;
+			System.out.print("Introduzca otro numero: ");
+			num = Entrada.entero();
+		}
+		System.out.println("se han introducido "+ cont+ " numeros.");
+	}
 }
